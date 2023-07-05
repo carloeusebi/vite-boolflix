9a4c16ba1d824e5916c4f159d4c8e8d4be6b17a9
carloeusebi <carloeusebi@gmail.com>
2023-07-05 17:42:46 +0200

    Added card overlay

--- src/components/AppPoster.vue
+++ src/components/AppPoster.vue
@@ -20,6 +20,14 @@ export default {
 				:alt="title"
 				onerror="this.style.display='none'" />
 			<p class="on-image-error">{{ title }}</p>
+			<div class="overlay">
+				<p><strong>Titolo:</strong> {{ title }}</p>
+				<p><strong>Titolo originale:</strong> {{ originalTitle }}</p>
+				<p><strong>Voto:</strong> {{ vote }}</p>
+				<div class="overview-container">
+					<p class="truncate"><strong>Trama:</strong> {{ overview }}</p>
+				</div>
+			</div>
 		</figure>
 	</div>
 </template>
@@ -28,14 +36,18 @@ export default {
 .poster {
 	cursor: pointer;
 	position: relative;
+	height: 450px;
 
 	img {
 		position: relative;
 		z-index: 1;
+		object-fit: cover;
+		height: 100%;
 	}
 
 	figure {
 		height: 100%;
+		widows: 100%;
 		background-color: #989898;
 		background-image: url('../assets/img/blank-background.png');
 		background-size: contain;
@@ -53,6 +65,33 @@ export default {
 	}
 
 	.overlay {
+		position: absolute;
+		inset: 0;
+		color: white;
+		background-color: rgba(0, 0, 0, 0.8);
+		z-index: 2;
+
+		font-size: 0.75rem;
+		padding: 3rem 0.5rem;
+
+		p {
+			margin: 0.5rem 0;
+		}
+
+		strong {
+			font-size: 0.9rem;
+		}
+
+		.overview-container {
+			max-height: 150px;
+			overflow: auto;
+		}
+
+		.truncate:not(:hover) {
+			text-overflow: ellipsis;
+			white-space: nowrap;
+			overflow: hidden;
+		}
 	}
 }
 </style>
