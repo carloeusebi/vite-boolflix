d3d0ee91dc6e43e66d70bb2e237375d4ae3762a6
carloeusebi <carloeusebi@gmail.com>
2023-07-07 16:52:50 +0200

    Prepared navbar

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,49 +1,61 @@
 <script>
-export default {};
+export default {
+	data() {
+		return {
+			links: ['Home', 'Serie Tv', 'Film'],
+			selectedLink: 'Home',
+		};
+	},
+	methods: {
+		handleClick(clickedLink) {
+			this.selectedLink = clickedLink;
+		},
+	},
+};
 </script>
 
 <template>
-  <nav>
-    <ul>
-      <li class="active">Home</li>
-      <li>Serie TV</li>
-      <li>Film</li>
-      <li>Originali</li>
-      <li>Aggiunti di recente</li>
-      <li>La mia lista</li>
-    </ul>
-  </nav>
+	<nav>
+		<ul>
+			<li
+				v-for="link in links"
+				@click="selectedLink = link"
+				:class="{ active: link === selectedLink }">
+				{{ link }}
+			</li>
+		</ul>
+	</nav>
 </template>
 
 <style lang="scss" scoped>
 @use '../assets/sass/vars' as *;
 
 ul {
-  display: flex;
+	display: flex;
 }
 
 li {
-  padding: 0 1rem;
-  font-weight: bold;
-  color: rgba(211, 211, 211, 0.575);
-  cursor: pointer;
+	padding: 0 1rem;
+	font-weight: bold;
+	color: rgba(211, 211, 211, 0.575);
+	cursor: pointer;
 
-  &.active {
-    color: white;
-  }
+	&.active {
+		color: white;
+	}
 
-  &:hover {
-    color: white;
-  }
+	&:hover {
+		color: white;
+	}
 }
 
 nav {
-  display: none;
+	display: none;
 }
 
 @media #{$media-laptop} {
-  nav {
-    display: block;
-  }
+	nav {
+		display: block;
+	}
 }
 </style>
